0735bb16a70413b33a90e7a093a5f8c8c1ab142e
michaelseptiadi <74148613+michaelseptiadi@users.noreply.github.com>
2022-03-30 16:51:50 +0700

    DIGITALENT calculator

---
+++ script.js
@@ -0,0 +1,93 @@
+const calculatorScreen = document.querySelector(".calculator-screen")
+const numbers = document.querySelectorAll(".number")
+const operators = document.querySelectorAll(".operator")
+const equalSign = document.querySelector(".equal-sign")
+const clearButton = document.querySelector(".all-clear")
+const decimal = document.querySelector(".decimal")
+
+let prevNumber = ""
+let calculationOperator = ""
+let currentNumber = "0"
+
+const updateScreen = (number) => {
+    calculatorScreen.value = number
+}
+
+const inputNumber = (number) => {
+    if (currentNumber === "0") {
+        currentNumber = number
+    } else {
+        currentNumber += number
+    }
+}
+
+const inputOperator = (operator) => {
+    if (calculationOperator === "") {
+        prevNumber = currentNumber
+    }
+    calculationOperator = operator
+    currentNumber = ""
+}
+
+const calculate = () => {
+    let result = ""
+    switch (calculationOperator) {
+        case "+":
+            result = parseFloat(prevNumber) + parseFloat(currentNumber)
+            break
+        case "-":
+            result = parseFloat(prevNumber) - parseFloat(currentNumber)
+            break
+        case "*":
+            result = parseFloat(prevNumber) * parseFloat(currentNumber)
+            break
+        case "/":
+            result = parseFloat(prevNumber) / parseFloat(currentNumber)
+            break
+        default:
+            break
+    }
+    currentNumber = result
+    calculationOperator = ""
+}
+
+const clearAll = () => {
+    prevNumber = ""
+    calculationOperator = ""
+    currentNumber = "0"
+}
+
+const inputDecimal = (dot) => {
+    if (currentNumber.includes(".")) {
+        return
+    }
+    currentNumber += dot
+}
+
+operators.forEach((operator) => {
+    operator.addEventListener("click", (event) => {
+        inputOperator(event.target.value)
+    })
+})
+
+numbers.forEach((number) => {
+    number.addEventListener("click", (event) => {
+        inputNumber(event.target.value)
+        updateScreen(currentNumber)
+    })
+})
+
+equalSign.addEventListener("click", () => {
+    calculate()
+    updateScreen(currentNumber)
+})
+
+clearButton.addEventListener("click", () => {
+    clearAll()
+    updateScreen(currentNumber)
+})
+
+decimal.addEventListener("click", (event) => {
+    inputDecimal(event.target.value)
+    updateScreen(currentNumber)
+})
